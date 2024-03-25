e19064da4980695c2730bfa29f43835b6fc64f6c
Jakob Niessner <jabruniessner@gmail.com>
2024-03-25 22:51:07 +0100

    Added example case

---
+++ example/example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#Performing the initial clustering
+../clust complexes_allgrids_c2c_nspec p2_noh.pdb 5 'a' 500 > stats.out
+
+#Getting increments, avd. ClSize, min. ClSize and max. ClSize columns
+../aux/complete_scoring.awk cluster_average_linkage.out > cluster_average_linkage_full_scoring.out
+
+#Plotting the results
+gnuplot -c ../aux/plot_cycles.gnu cluster_average_linkage_full_scoring.out plot.pdf "bnbs docking" 500
