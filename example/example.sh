c969ee2e10a31a7fab40faf270a5d55b4de9016c
jabruniessner <jabruniessner@gmail.com>
2024-04-09 15:09:01 +0200

    Adapted the example.sh file

--- example/example.sh
+++ example/example.sh
@@ -2,7 +2,11 @@
 
 
 #Performing the initial clustering
-../clust complexes_allgrids_c2c_nspec p2_noh.pdb 5 'a' 500 > stats.out
+../clust complexes_allgrids_c2c_nspec p2_noh.pdb 5 'a' 500
+
+#Doing the treecutting 
+
+../cut cluster_average_linkage_full_scoring.out complexes_allgrids_c2c_nspec p2_noh.pdb 5 'a' > stats.out
 
 #Getting increments, avd. ClSize, min. ClSize and max. ClSize columns
 ../aux/complete_scoring.awk cluster_average_linkage.out > cluster_average_linkage_full_scoring.out
